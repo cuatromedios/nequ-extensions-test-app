35ba12bf454406698202275be8ca7c81f0e5f889
Ramses <ramses@cuatromedios.com>
2020-02-03 14:32:36 -0600

    Slot inheritance in generic input

--- src/layouts/MainLayout.vue
+++ src/layouts/MainLayout.vue
@@ -36,7 +36,7 @@
       </q-list>
     </q-drawer>
 
-    <q-page-container>
+    <q-page-container class="q-ma-lg">
       <router-view />
     </q-page-container>
   </q-layout>
--- src/pages/Input.vue
+++ src/pages/Input.vue
@@ -1,6 +1,10 @@
 <template>
-  <q-page class="">
-    <h3>Input</h3>
+  <q-page>
+    <h1>Input</h1>
+    <h2>Default</h2>
+    Outlined by default
+    <nq-input></nq-input>
+    <h2>Inherit scopes</h2>
     <nq-input>
       <template v-slot:append>
         <q-icon name="event" color="orange" />
