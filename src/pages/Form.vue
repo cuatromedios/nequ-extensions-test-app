f99968a69c51463daa08dbb33d48f397a4154a04
Ramses <ramses@cuatromedios.com>
2020-02-06 13:53:15 -0600

    NqForm used in a Page with the NqFormMixin. Waiting to connect this to the API wrapper.

--- src/pages/Form.vue
+++ src/pages/Form.vue
@@ -1,20 +1,45 @@
 <template>
   <q-page class="">
+    <h1>NqFormMixin</h1>
+    <p>A mixin with default methods to handle a Form</p>
+    <h2>Data</h2>
+    <ul>
+      <li><code>submitting</code> True if the page is submitting data. This value is set in the default onSubmit() method in the mixin. Need to be set manually if onSumbit() is overwritten</li>
+    </ul>
+    <h2>Methods</h2>
+    <ul>
+      <li><code>onSumbit</code> Will take the form data, and send it to the defined ennpoint </li>
+      <li><code>submitting</code> Boolean: Default to false, If the data is beign sent, disables the form and shows a loading spinner</li>
+    </ul>
     <h1>&lt;nq-form&gt;</h1>
+    <p>A component that extends <code>q-form</code></p>
     <h2>Features</h2>
     <ul>
       <li>Validation</li>
-      <li>Uses a <code>form</code> mixin</li>
+      <li>Can be used with a vue template that extends the <code>form</code> mixin</li>
       <li>Automatically set a .row class and .q-col-gutter-md so fields may add its own col-X class</li>
       <li>Adds a default submit and cancel button (customizable labels)</li>
+    </ul>
+    <h2>Attributes</h2>
+    <ul>
+      <li><code>ready</code> Boolean: Default to true, If the form is or not ready to receive input. Usefult if need to load something before showing the form. Will hide the from if true and show a skleton.</li>
+      <li><code>submitting</code> Boolean: Default to false, If the data is being sent, disables the form and shows a loading spinner in the submit button. Can be tied to the submitting property of the page containing the form</li>
+      <li><code>submit-label</code> The label to use in the submit button</li>
+      <li><code>cancel-label</code> The label to use in the cancel button, if set to '' the button will not show</li>
+    </ul>
+    <h2>Events</h2>
+    <ul>
+      <li><code>@submit</code> Triggered when the form is submited, need to be listened in a page method, for example onSubmit method in the mixin.</li>
+      <li><code>@cancel</code> Triggered when the form is canceled, need to be listened in a page method to know what to do</li>
     </ul>
     <hr><br>
     <nq-form
       @submit="onSubmit"
       @cancel="onCancel"
       submit-label="save"
+      cancel-label="back"
       :ready="!loading"
-      :saving="saving"
+      :submitting="submitting"
     >
       <nq-input
         class="col-12"
@@ -51,8 +76,10 @@
 </template>
 
 <script>
+import { NqFormMixin } from '../mixins/NqFormMixin'
 export default {
   name: 'Currency',
+  mixins: [ NqFormMixin ],
   data () {
     return {
       form: {
@@ -60,18 +87,15 @@ export default {
         age: null,
         accept: false
       },
-      saving: false,
       loading: true
     }
   },
   mounted() {
-    setTimeout(() => this.loading = false, 2000)
+    this.loadEndpoints()
   },
   methods: {
-    onSubmit () {
-      this.saving = true
-      console.log('onSubmit')
-      setTimeout(() => this.saving = false, 3000)
+    onSubmitOk () {
+
     },
     onCancel () {
       console.log('onCancel')
